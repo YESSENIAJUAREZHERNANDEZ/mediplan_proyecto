a3bdf6dff06e9e0ce4d132f2f867838fe7c2f24b
mediplan <1720110342@utectulancingo.edu.mx>
2023-07-21 17:08:57 -0600

    finish

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/barrita.dart';
 //import 'package:flutter_application_1/screens/alarma.dart';
 import 'package:flutter_application_1/screens/camara.dart';
 import 'package:flutter_application_1/screens/dispositivos.dart';
 import 'package:flutter_application_1/screens/editarecordatorios.dart';
 import 'package:flutter_application_1/screens/editarmedicamento.dart';
-import 'package:flutter_application_1/screens/formulario.dart';
+//import 'package:flutter_application_1/screens/formulario.dart';
 import 'package:flutter_application_1/screens/loginapp.dart';
 //import 'package:flutter_application_1/screens/medicamentos.dart';
 import 'package:flutter_application_1/screens/tratamiento.dart';
@@ -99,7 +100,7 @@ class MainScreen extends StatelessWidget{
           onTap: () {
             // Acción para la opción 3
             Navigator.push( context,
-                  MaterialPageRoute(builder: (context) => ReminderPage()),
+                  MaterialPageRoute(builder: (context) => Medications2()),
                 );
           },
         ),
