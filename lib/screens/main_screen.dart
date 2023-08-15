a5dc7c940bbe1a1cee3ea5d4ee7521a97043db2f
mediplan <1720110342@utectulancingo.edu.mx>
2023-08-15 03:10:18 -0600

    reconocimiento texto

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter_application_1/screens/seguimiento.dart';
 import 'package:flutter_application_1/screens/splash_dos.dart';
 import 'package:flutter_application_1/screens/splash_screen.dart';
 import 'package:flutter_application_1/screens/terminos.dart';
+import 'package:flutter_application_1/text/inicio.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -328,7 +329,7 @@ Widget _forYou(context) {
               // Acción cuando se presiona el tercer botón
               Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Noti()),
+                        MaterialPageRoute(builder: (context) => Plan()),
                       );
               
             },
---
+++ lib/text/inicio.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/text/home_page.dart';
+
+void main() => runApp(const Plan());
+
+class Plan extends StatelessWidget {
+  const Plan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: HomePage(),
+    );
+  }
+}
